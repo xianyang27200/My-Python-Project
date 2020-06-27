c214e327b3628c1c5e66fd6e6432b7d793f935fa
xianyang27200 <49306745+xianyang27200@users.noreply.github.com>
2020-06-27 13:09:01 +0800

    database added

---
+++ contact book/pytest/contact_book.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for macos10.15 (x86_64)
+--
+-- Host: sql12.freemysqlhosting.net    Database: sql12350647
+-- ------------------------------------------------------
+-- Server version	5.5.62-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contact_details`
+--
+
+DROP TABLE IF EXISTS `contact_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contact_details` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Phone` varchar(50) NOT NULL,
+  `Address` text NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contact_details`
+--
+
+LOCK TABLES `contact_details` WRITE;
+/*!40000 ALTER TABLE `contact_details` DISABLE KEYS */;
+INSERT INTO `contact_details` VALUES (1,'Muhumuza','+250 788 526 814','rusizi','muh@yahoo.fr','2020-06-25'),(2,'wg','+86 132 4784 7432','xian','wg@gmail.com','2020-06-26'),(3,'lubuto','+86 132 2907 8302','xianyangweinroad2','lub@yahoo.fr','2020-06-26');
+/*!40000 ALTER TABLE `contact_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-26 16:02:14
